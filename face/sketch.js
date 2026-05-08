07c536de870836bdae596dbe8490420f43a4a980
bjw0112 <23609947a@gmail.com>
2026-05-08 16:02:36 +0900

    Add files via upload

---
+++ face/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  background(220);
+  fill(255,240,240);
+  ellipse(200,200,300,350);
+  
+  ellipse(135,150, 80,40);
+  ellipse(270,150, 80,40);
+  fill(0,0,0);
+  ellipse(135,150, 40,40);
+  ellipse(270,150, 40,40);
+  triangle(200,160,175,255,225,255);
+  ellipse(200,300,100,2);
+  fill(0,0,0);
+  ellipse(200,75,270,100);
+}
